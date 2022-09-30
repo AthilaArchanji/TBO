84f31473644e4ff3855b12043b0ef4657d828c29
Athila Archanji <82385857+AthilaArchanji@users.noreply.github.com>
2022-09-30 10:26:12 -0300

    criando o a função de selectionSort

--- selectionSort/main.c
+++ selectionSort/main.c
@@ -2,6 +2,29 @@
 #include <stdio.h>
 #include <time.h>
 
+void selecao(int *vet, int n)
+{
+
+    int Menor, aux;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        Menor = i;
+        
+        for (int j = i + 1; j < n; j++)
+        {
+            if (vet[Menor] > vet[j])
+                Menor = j;
+        }
+        if (i != Menor)
+        {
+            aux = vet[i];
+            vet[i] = vet[Menor];
+            vet[Menor] = aux;
+        }
+    }
+}
+
 int main()
 {
     int N, *vet, i;
@@ -18,5 +41,12 @@ int main()
     for (i = 0; i < N; i++)
     {
         printf("%d ", *(vet + i));
+    }
+
+    selecao(vet, N);
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d - ", *(vet + i));
     }
 }
